ee0c350f3b5565057b42661dfcbe75c95aceebb8
holding-1-at-a-time <rromerojr1@gmail.com>
2024-10-20 10:03:21 -0500

    "Added sourcery package and updated pnpm-lock.yaml"

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@
     * @description      : 
     * @author           : Owner
     * @group            : 
-    * @created          : 20/10/2024 - 06:25:31
+    * @created          : 20/10/2024 - 09:52:31
     * 
     * MODIFICATION LOG
     * - Version         : 1.0.0
@@ -10,8 +10,8 @@
     * - Author          : Owner
     * - Modification    : 
 **/
-// convex/client.ts
-import { ConvexClient } from "convex/browser";
-import { api } from "./api";
+import { SignUp } from '@clerk/nextjs'
 
-export const convex = new ConvexClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+export default function Page() {
+    return <SignUp />
+}
